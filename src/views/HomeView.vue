af638a603f94cfdec67b2063b6ce4d75019465db
brysonbw <brysonward77@gmail.com>
2023-04-01 11:55:52 -0400

    refactored TodoList component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,10 @@
   <v-container class="black--text my-12">
     <v-row class="text-center">
       <v-col>
+        <!-- Showcase Title/Text-->
         <h1 class="display-2 font-weight-bold mb-10">Vue Todo</h1>
         <!-- Todo List -->
-        <TodoList />
+        <TodoList :todoList="todoList" />
       </v-col>
     </v-row>
   </v-container>
@@ -17,5 +18,10 @@ export default Vue.extend({
   name: 'HomeView',
 
   components: { TodoList },
+  data() {
+    return {
+      todoList: [],
+    };
+  },
 });
 </script>
