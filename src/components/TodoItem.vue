6b58a78505682d65599e71a6a2b5f260e4edea4c
brysonbw <brysonward77@gmail.com>
2023-04-01 11:54:14 -0400

    added TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-left my-3">
+    <v-list-item @click="$emit('toggleDone')">
+      <template v-slot:default>
+        <v-list-item-action>
+          <v-checkbox :input-value="todo.done"></v-checkbox>
+        </v-list-item-action>
+
+        <v-list-item-content>
+          <v-list-item-subtitle class="font-weight-bold">{{
+            todo.body
+          }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-icon @click.stop="$emit('deleteTodo')">mdi-delete</v-icon>
+      </template>
+    </v-list-item>
+    <v-divider></v-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['todo'],
+};
+</script>
+
+<style></style>
