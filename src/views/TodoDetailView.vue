0565c1bca713bf3b45195ca8d76b394f507148b5
brysonbw <brysonward77@gmail.com>
2023-04-03 20:08:39 -0400

    cleaned up TodoDetail - fetch todo by ID

--- src/views/TodoDetailView.vue
+++ src/views/TodoDetailView.vue
@@ -3,8 +3,8 @@
     <v-row class="text-center">
       <v-col>
         <h1 class="display-2 font-weight-bold mb-10">Todo</h1>
-        <h1 class="display-1 font-weight-bold mb-5">{{ body }}</h1>
-        <p class="mb-10">{{ convertDate(createdAt) }}</p>
+        <h1 class="display-1 font-weight-bold mb-5">{{ todo.body }}</h1>
+        <p class="mb-10">{{ convertDate(todo.createdAt) }}</p>
         <v-btn @click="$router.push('/')">Go back</v-btn>
       </v-col>
     </v-row>
@@ -14,7 +14,17 @@
 <script>
 import moment from 'moment';
 export default {
-  props: ['id', 'body', 'createdAt'],
+  props: ['id'],
+  data() {
+    return {
+      todo: null,
+    };
+  },
+  created() {
+    const todoList = JSON.parse(localStorage.getItem('todoList'));
+    const todo = todoList.find((item) => item.id === this.$route.params.id);
+    return (this.todo = todo);
+  },
   methods: {
     convertDate(date) {
       return moment(date).format('LL');
