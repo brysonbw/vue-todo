7b20d261a57b15f498a43690c667d8fb8a37e5dd
brysonbw <brysonward77@gmail.com>
2023-04-16 01:57:13 -0400

    added vuex store typings

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,19 @@
+import { Todo } from './types/todo';
+import { Snackbar } from './types/snackbar';
+
 declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
+}
+
+// typing vuex and $store property
+// vuex docs: https://vuex.vuejs.org/guide/typescript-support.html
+
+declare module '@vue/runtime-core' {
+  // declaring store states
+  export interface State {
+    snackbar: Snackbar;
+    todo: Todo;
+    todos: Todo[];
+  }
 }
